0573f17732cfa157cd9b849cb1e0bd005ba4ff59
dado1945 <dado1945@7bbd8241-a1f8-0310-88b3-d2246e109db5>
2005-09-14 18:12:49 +0000

    Do not generate rss for index file

--- src/rss.py
+++ src/rss.py
@@ -3,6 +3,8 @@ from docutils.core import publish_parts
 import time
 import locale
 
+tr = lambda s: s.replace('&', '&amp;').replace('<','&lt;').replace('>','&gt;')
+
 def generateRSS(root, cfg, entries):
     text = ''
     text += '<?xml version="1.0" encoding="utf-8"?>'
@@ -14,36 +16,35 @@ def generateRSS(root, cfg, entries):
             (cfg.get('blog', 'name')[1], cfg.get('blog', 'webroot')[1])
 
     for entry in entries[:10]:
-        text += '<item>'
-        text += '<title> %s </title>' % entry["title"]
-        text += '<link>http://%s.html'\
-              '</link>' % join(cfg.get('blog', 'webroot')[1],
-                               root, entry['name'])
+        if entry['name'] != 'index':
+            text += '<item>'
+            text += '<title> %s </title>' % entry["title"]
+            text += '<link>http://%s.html'\
+                  '</link>' % join(cfg.get('blog', 'webroot')[1],
+                                   root, entry['name'])
 
-        locale.setlocale(locale.LC_ALL, cfg.get('blog', 'locale')[1])
-        rst = ''
-        if len(entry['log']) == 1:
-            rst = change['msg']
-        else:
-            for change in entry['log']:
-                if len(change['msg']) > 0:
-                    rst += time.strftime(cfg.get('blog', 'timestamp')[1],
-                                                 change['date']) + '\n'
-                    for line in change['msg'].split('\n'):
-                        rst += ' '*4 + line + '\n'
-                    rst += '\n'
+            locale.setlocale(locale.LC_ALL, cfg.get('blog', 'locale')[1])
+            rst = ''
+            if len(entry['log']) == 1:
+                rst = change['msg']
+            else:
+                for change in entry['log']:
+                    if len(change['msg']) > 0:
+                        rst += time.strftime(cfg.get('blog', 'timestamp')[1],
+                                                     change['date']) + '\n'
+                        for line in change['msg'].split('\n'):
+                            rst += ' '*4 + line + '\n'
+                        rst += '\n'
 
-        parts = publish_parts(rst, writer_name='html')
+            parts = publish_parts(rst, writer_name='html')
 
-        tr = lambda s: s.replace('&', '&amp;').replace('<','&lt;').replace('>','&gt;')
-
-        text += '<description>%s</description>' % \
-                tr(parts['html_body'].encode('utf-8'))
-        locale.setlocale(locale.LC_ALL, 'C')
-        text += '<pubDate>%s</pubDate>' % \
-                time.strftime("%a, %d %b %Y %H:%M:%S GMT",
-                              entry['log'][-1]['date'])
-        text += '</item>'
+            text += '<description>%s</description>' % \
+                    tr(parts['html_body'].encode('utf-8'))
+            locale.setlocale(locale.LC_ALL, 'C')
+            text += '<pubDate>%s</pubDate>' % \
+                    time.strftime("%a, %d %b %Y %H:%M:%S GMT",
+                                  entry['log'][-1]['date'])
+            text += '</item>'
 
     text += '</channel>'
     text += '</rss>'
